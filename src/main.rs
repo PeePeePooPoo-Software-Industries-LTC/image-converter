1c1100d14ee1a1cd09678ff244e351801e29c793
jaschutte <34577095+jaschutte@users.noreply.github.com>
2023-12-18 15:28:22 +0100

    Changed 565 RGB into 10 bit RGB

--- src/main.rs
+++ src/main.rs
@@ -15,20 +15,8 @@ const RANGE_SIG_BIT: u8 = 0x80;
 const RANGE_COL_OFFSET: u8 = 0;
 const RANGE_RANGE_OFFSET: u8 = 3;
 
-macro_rules! rgb565_to_u16 {
-    ($r:expr, $g:expr, $b:expr) => {
-        ($r * 2048 + $g * 32 + $b) as u16
-    };
-}
-
-macro_rules! rgb_to_565 {
-    ($r:expr, $g:expr, $b:expr) => {
-        (
-            ($r as f32 / 255.0 * 31.0) as u16,
-            ($g as f32 / 255.0 * 63.0) as u16,
-            ($b as f32 / 255.0 * 31.0) as u16,
-        )
-    };
+fn convert_to_10bit_rgb(r: u8, g: u8, b: u8) -> u32 {
+    (((r << 2) as u32) << 20) | (((g << 2) as u32) << 10) | (b as u32)
 }
 
 #[derive(Clone, Copy)]
@@ -43,7 +31,7 @@ impl PixelRange {
     }
 }
 
-fn get_palette_index(color_palette: &mut [Option<u16>; 8], pixel_color: u16) -> Option<usize> {
+fn get_palette_index(color_palette: &mut [Option<u32>; 8], pixel_color: u32) -> Option<usize> {
     for (idx, palette_color) in color_palette.iter_mut().enumerate() {
         match palette_color {
             Some(palette_color) => {
@@ -73,7 +61,6 @@ fn main() {
     	.save_file() else { panic!("No save location selected.") };
 
     let mut all_content = String::new();
-    let mut all_content_cpp = String::new();
     for file in files {
         // That's gotta be the worst rust code I've ever seen
         // So it would seem
@@ -95,7 +82,7 @@ fn main() {
         all_content += &format!("\n// AUTO-GENERATED IMAGE CONVERTED FROM: {}\n", file_name);
 
         const WINDOW_SIZE: usize = 4;
-        let mut color_palette: [Option<u16>; OPT_MAX_COLORS] = [None; OPT_MAX_COLORS];
+        let mut color_palette: [Option<u32>; OPT_MAX_COLORS] = [None; OPT_MAX_COLORS];
 
         let mut prev_pixel = IMPOSSIBLE_INDEX;
         let mut matching_pixels = 1;
@@ -169,8 +156,7 @@ fn main() {
         // Get a color palette from the raw image
         let get_color_palette = |(pixel_x, pixel_y, pixel): (u32, u32, &Rgb<u8>)| {
             let pixel = pixel.0;
-            let (r, g, b) = rgb_to_565!(pixel[0], pixel[1], pixel[2]);
-            let color = rgb565_to_u16!(r, g, b);
+            let color = convert_to_10bit_rgb(pixel[0], pixel[1], pixel[2]);
 
             match get_palette_index(&mut color_palette, color) {
                 Some(idx) => idx as u8,
@@ -199,42 +185,27 @@ fn main() {
 
         // Convert the image data into a C array
         let (w, h) = rgb.dimensions();
-        let image_len = image_data.len() + 16;
-        all_content_cpp += &format!(
-            "// AUTO GENERATED IMAGE, PUT ME IN images.cpp
-RawImage image_{file_stem} {{
-	GET_IMAGE({file_stem}),
-	{image_len},
-	Vector2 {{ {w}, {h} }}
-}};
-",
-        );
-        all_content += "// AUTO GENERATED IMAGE, PUT ME IN images.h\n";
-        all_content += &format!("extern RawImage image_{};\n", file_stem,);
-        all_content += &format!("PROGMEM const char __raw_{file_stem}_p[{image_len}] = {{\n\t",);
-        for color in color_palette {
-            all_content += &format!(
-                "0x{:02x}, 0x{:02x}, ",
-                match color {
-                    Some(c) => c >> 8,
-                    None => 0,
-                },
-                match color {
-                    Some(c) => c & 0xFF,
-                    None => 0,
-                },
+        let image_len = image_data.len();
+        all_content += &format!("unsigned char image_{file_stem}_width = {w};\r\n");
+        all_content += &format!("unsigned char image_{file_stem}_height = {h};\r\n");
+        let palette_series = color_palette.into_iter().fold(String::new(), |str, color| {
+            match color {
+                Some(color) => format!("{str}0x{color:08x}, "),
+                None => format!("{str}0x00000000, "),
+            }
+        });
+        all_content += &format!("unsigned int image_{file_stem}_palette[8] = {{\r\n\t{palette_series}\r\n}};\r\n");
+        all_content += &format!("unsigned char image_{file_stem}[{image_len}] = {{\r\n\t");
+        let byte_series = image_data.into_iter().enumerate().fold(String::new(), |mut str, (index, byte)| {
+            str += &format!(
+                "0x{:02x},{}",
+                byte,
+                if index as u32 % 10 == 0 && index > 0 { "\r\n\t" } else { " " }
             );
-        }
-        all_content.pop();
-        all_content += "\n\t";
-        for encoded_pixel in &image_data {
-            all_content += &format!("0x{:02x}, ", *encoded_pixel);
-        }
-        all_content.pop();
-        all_content.pop();
-        all_content += "\n};\n";
+            str
+        });
+        all_content += &format!("{byte_series}\r\n}};\r\n\r\n");
     }
-    all_content += all_content_cpp.as_str();
 
     match std::fs::write(&output, &all_content) {
         Ok(_) => {
